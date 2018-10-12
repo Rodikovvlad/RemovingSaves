35f1dc769398747dab9f4ae37bc98a2b1b042cfb
Vlad <39596326+Rodikovvlad@users.noreply.github.com>
2018-10-12 13:01:46 +0700

    Add files via upload

--- removesaves.py
+++ removesaves.py
@@ -4,19 +4,14 @@ path = "C:/Users/Влад\Documents/The Witcher/saves"
 saves = os.listdir(path)
 
 checks = []
-perf = []
+
 
 for save in saves:
-    checks.append(save[3:6])
-
-for num in checks:
-    perf.append(int(num[::-1]))
-
-maximum = max(perf)
-index = perf.index(maximum)
+    if save[4] == '0':
+        checks.append(int(save[5]))
+    else:
+        checks.append(int(save[4:6]))
 
 for save in saves:
-    if saves.index(save) != index:
+    if saves.index(save) != checks.index(max(checks)):
         os.remove(path+"/"+save)
-
-print(os.listdir(path))
